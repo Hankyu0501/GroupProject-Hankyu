8d3e24c8bb2aa4533a0f1555f5583a4ae234e334
Hankyu Shin <brad0501@naver.com>
2020-11-07 16:10:00 -0500

    upload

---
+++ data.js
@@ -0,0 +1,31 @@
+module.exports = function () {
+  return {
+    "book-list": [
+      {
+        id: 1,
+        name: "Harry Potter and Philosopher's Stone",
+        author: "J. K. Rowling",
+        publised: "1997",
+        description: "description",
+        price: 43.95,
+      },
+      {
+        id: 2,
+        name: "And Then There Were None",
+        author: "Agatha Christie",
+        published: "1939",
+        description: "description",
+        price: 39.95,
+      },
+      {
+        id: 3,
+        name: "Murder on the Orient Express",
+        author: "Agatha Christie",
+        published: "1934",
+        description: "description",
+        price: 99.33,
+      }
+    ],
+    orders: []
+  };
+};
